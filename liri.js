363eff014de8d444e70e346582b8157d644d4c46
samlutz <sammlutz@gmail.com>
2019-04-09 14:22:18 -0400

    added node packages

--- liri.js
+++ liri.js
@@ -2,5 +2,11 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var Spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
 
+var moment = require('moment');
+moment().format();
+
+var axios = require("axios");

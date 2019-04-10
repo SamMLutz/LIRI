6a3b1b7c0a53f88ee525197f44ba441cff1d9eb5
samlutz <sammlutz@gmail.com>
2019-04-09 20:35:59 -0400

    movie-this

--- liri.js
+++ liri.js
@@ -10,3 +10,27 @@ var moment = require('moment');
 moment().format();
 
 var axios = require("axios");
+
+var nodeArg = process.argv;
+
+// have liri take in one of several commands
+
+var commandArray = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"];
+
+if (nodeArg[2] === commandArray[2]) {
+    var movie = nodeArg[3];
+
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    
+    // console.log(response)
+    console.log(response.data.Title);
+    console.log("Release year: " + response.data.Year);
+    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+    console.log("The movie was produced in: " + response.data.Country);
+    console.log("The movie is in: " + response.data.Language);
+    console.log("plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
+  }
+);
+}

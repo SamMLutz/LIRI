2e1f9bffb7819d8681c30943db52179058bd06ec
samlutz <sammlutz@gmail.com>
2019-04-09 21:09:24 -0400

    checkpoint

--- liri.js
+++ liri.js
@@ -21,16 +21,25 @@ if (nodeArg[2] === commandArray[2]) {
     var movie = nodeArg[3];
 
     axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    
-    // console.log(response)
-    console.log(response.data.Title);
-    console.log("Release year: " + response.data.Year);
-    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
-    console.log("The movie was produced in: " + response.data.Country);
-    console.log("The movie is in: " + response.data.Language);
-    console.log("plot: " + response.data.Plot);
-    console.log("Actors: " + response.data.Actors);
-  }
-);
+        function (response) {
+
+            // console.log(response)
+            console.log(response.data.Title);
+            console.log("Release year: " + response.data.Year);
+            console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+            console.log("The movie was produced in: " + response.data.Country);
+            console.log("The movie is in: " + response.data.Language);
+            console.log("plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }
+    );
+} else if (nodeArg[2] === commandArray[0]) {
+    var artist = nodeArg[3];
+    var endpoint = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    axios.get(endpoint).then(
+        function (res) {
+            console.log(res.data.offers.venue.name);
+        }
+    )
 }
